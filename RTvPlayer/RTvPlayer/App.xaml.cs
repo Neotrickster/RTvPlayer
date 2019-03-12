3e1238607fe932f0d06daaaca441a785d71de1f2
Neotrickster <juanfchavezl@yahoo.com>
2019-03-12 17:45:35 -0600

    Esta app tiene la interface doble, la 2.3 y la que parece como Fx Sports

--- RTvPlayer/RTvPlayer/App.xaml.cs
+++ RTvPlayer/RTvPlayer/App.xaml.cs
@@ -9,7 +9,7 @@ namespace RTvPlayer
 		{
 			InitializeComponent();
 
-            MainPage = new RTvCanales(); //RadioTelevisionCanales(); //IberoamericaRadio(); //CanalIberoamericano(); //RTvCanales();
+            MainPage = new RadioTelevisionCanales(); //RadioTelevisionCanales(); //IberoamericaRadio(); //CanalIberoamericano(); //RTvCanales();
             //MainPage = new NavigationPage(new RadioTelevisionCanales());
             //NavigationPage.SetHasNavigationBar(MainPage, false);
         }
